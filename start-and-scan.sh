b5e9f639acafd8b7889c76798e24c321143fd301
Antti Virtanen <antti.virtanen@solita.fi>
2017-10-13 00:37:36 +0300

    active attack scan

--- start-and-scan.sh
+++ start-and-scan.sh
@@ -3,4 +3,8 @@
 
 export FLASK_APP=flask-vuln.py
 flask run --host=172.17.0.1 &
-docker run -t owasp/zap2docker-stable zap-baseline.py  -j -t http://172.17.0.1:5000
+# baseline scan (passive crawling)
+# docker run -t owasp/zap2docker-stable zap-baseline.py  -j -t http://172.17.0.1:5000
+
+# active quick attack scan
+docker run -i owasp/zap2docker-stable zap-cli quick-scan --self-contained --start-options '-config api.disablekey=true' --spider -r -s xss  http://172.17.0.1:5000

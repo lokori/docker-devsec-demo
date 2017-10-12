85bab6ca99c97f33fe463382087779211c86b6e5
Antti Virtanen <antti.virtanen@solita.fi>
2017-10-12 23:37:01 +0300

    forgot to install flask

--- start-and-scan.sh
+++ start-and-scan.sh
@@ -3,4 +3,4 @@
 
 export FLASK_APP=flask-vuln.py
 flask run --host=172.17.0.1 &
-docker run -t owasp/zap2docker-stable zap-baseline.py -r report.html -j -t http://172.17.0.1:5000
+docker run -t owasp/zap2docker-stable zap-baseline.py  -j -t http://172.17.0.1:5000

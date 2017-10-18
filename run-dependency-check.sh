a406e2137c281fe1a9a262292efd042cefebcfa9
Antti Virtanen <antti.virtanen@solita.fi>
2017-10-18 14:50:41 +0300

    report generation fix

--- run-dependency-check.sh
+++ run-dependency-check.sh
@@ -4,7 +4,8 @@
 
 OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
-REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+#REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+REPORT_DIRECTORY= $(pwd)/reports
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directories"
@@ -19,7 +20,7 @@ fi
 docker pull owasp/dependency-check
 
 docker run --rm \
-    --volume $(pwd):/src \
+    --volume $(pwd)/target:/src \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
     --volume "$REPORT_DIRECTORY":/report \
     owasp/dependency-check \
@@ -28,6 +29,8 @@ docker run --rm \
     --project "My OWASP Dependency Check Project"
     # Use suppression like this: (/src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
+
+ls -altr reports
 
 # Force "ok" for Travis
 exit 0

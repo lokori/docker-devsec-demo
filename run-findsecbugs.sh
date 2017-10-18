f9d9b884bc46b5ae41e5f151ae0b4fb8b7aa7263
Antti Virtanen <antti.virtanen@solita.fi>
2017-10-18 11:00:43 +0300

    fix src directory for findsec bugs

--- run-findsecbugs.sh
+++ run-findsecbugs.sh
@@ -3,7 +3,7 @@
 # https://github.com/lokori/findbugs-security-docker
 
 docker pull lokori/findbugs-sec
-docker run  -v `pwd`/src/java:/workdir/src lokori/findbugs-sec src
+docker run  -v `pwd`:/workdir/src lokori/findbugs-sec src
 
 # getting the report out of the container use something like this
 # -html -output /workdir/src/findsec-report.html src
